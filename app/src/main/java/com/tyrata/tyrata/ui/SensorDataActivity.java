c24896478df00ef424a9ebce342ae513e993c096
JMPsuit <mstangler@Tyrata.lan>
2019-07-03 08:34:58 -0400

    Changed the scanning time

--- app/src/main/java/com/tyrata/tyrata/ui/SensorDataActivity.java
+++ app/src/main/java/com/tyrata/tyrata/ui/SensorDataActivity.java
@@ -412,12 +412,16 @@ public class SensorDataActivity extends Activity implements ScanResultsConsumer 
         if(!isAD7747) {
             try {
                 isAfterReading = true;
+
+                 // Doesn't run, tablet crashes
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         ToasterService.makeToast(SensorDataActivity.this, Constants.READING,90000);
                     }
-                });Thread.sleep(90000);
+
+
+                });Thread.sleep(43000);
                 bluetooth_le_adapter.connect(device_address);
             } catch (InterruptedException e) {
                 e.printStackTrace();
